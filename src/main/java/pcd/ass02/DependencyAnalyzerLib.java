3af1a73b245a71599ac76b79decb33bc99d1124c
erattini <emiliano.rattini@studio.unibo.it>
2025-04-29 18:53:15 +0200

    Implement get class and package dependencies

---
+++ src/main/java/pcd/ass02/DependencyAnalyzerLib.java
@@ -0,0 +1,81 @@
+package pcd.ass02;
+
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.ImportDeclaration;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.file.FileSystem;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class DependencyAnalyzerLib {
+
+    public record ClassDepsReport(List<String> dependencies){}
+
+    public static Future<ClassDepsReport> getClassDependencies(String source) {
+        return Vertx.vertx().fileSystem().exists(source)
+                .compose(exist -> {
+                    if (exist) {
+                        return Future.succeededFuture(new File(source));
+                    }
+                    else {
+                        return Future.failedFuture(new IllegalArgumentException("File doesnt exist"));
+                    }
+                }).compose(file -> {
+                    try {
+                        return Future.succeededFuture(StaticJavaParser.parse(file)
+                                .getImports().stream()
+                                .map(ImportDeclaration::getName)
+                                .map(Objects::toString)
+                                .collect(Collectors.toList()));
+                    } catch (FileNotFoundException e) {
+                        return Future.failedFuture(e);
+                    }
+                }).map(ClassDepsReport::new);
+    }
+
+    public static Future<ClassDepsReport> getClassDependenciesRaw(File source) {
+        return Vertx.vertx().executeBlocking(() -> {
+            CompilationUnit unit = StaticJavaParser.parse(source);
+            List<String> imports = unit.getImports().stream()
+                    .map(ImportDeclaration::getName)
+                    .map(Objects::toString)
+                    .collect(Collectors.toList());
+            return new ClassDepsReport(imports);
+        });
+    }
+
+    public record PackageDepsReport(List<ClassDepsReport> classReports){}
+
+    public static Future<PackageDepsReport> getPackageDependencies(String source) {
+        FileSystem fileSystem = Vertx.vertx().fileSystem();
+        return fileSystem
+                .exists(source)
+                .compose(exist -> {
+                    if (exist) {
+                        return fileSystem.props(source);
+                    } else {
+                        return Future.failedFuture(new IllegalArgumentException("File doesnt exist"));
+                    }
+                })
+                .compose(fileProps -> {
+                    if (fileProps.isDirectory()) {
+                        return fileSystem.readDir(source);
+                    } else {
+                        return Future.failedFuture(new IllegalArgumentException("File is not a directory"));
+                    }
+                })
+                .compose(paths -> {
+                    List<Future<ClassDepsReport>> classReports = paths.stream()
+                            .map(DependencyAnalyzerLib::getClassDependencies)
+                            .collect(Collectors.toList());
+                    return Future.all(classReports);
+                })
+                .map(compositeFuture -> new PackageDepsReport(compositeFuture.list()));
+    }
+}
