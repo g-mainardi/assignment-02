2c503e8894624c928fa8d2dd853b1f9f61230983
erattini <emiliano.rattini@studio.unibo.it>
2025-04-30 14:49:48 +0200

    Improve get package dependecies

--- src/main/java/pcd/ass02/DependencyAnalyzerLib.java
+++ src/main/java/pcd/ass02/DependencyAnalyzerLib.java
@@ -4,13 +4,16 @@ import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.ImportDeclaration;
 import io.vertx.core.Future;
+import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.file.FileSystem;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class DependencyAnalyzerLib {
@@ -30,6 +33,26 @@ public class DependencyAnalyzerLib {
     }
 
     public record PackageDepsReport(List<ClassDepsReport> classReports){}
+
+    public static Future<PackageDepsReport> getPackageDependencies(File source) {
+        Promise<PackageDepsReport> promise = Promise.promise();
+        vertx.executeBlocking(() -> {
+                    if (!source.isDirectory()) {
+                        throw new IllegalArgumentException("Not dir");
+                    }
+                    File[] files = source.listFiles();
+                    if (files == null) {
+                        throw new IOException("List files returned null");
+                    }
+                    List<Future<ClassDepsReport>> classReports = Arrays.stream(files)
+                            .filter(File::isFile)
+                            .map(DependencyAnalyzerLib::getClassDependencies)
+                            .toList();
+                    return Future.all(classReports);
+                })
+                .onSuccess(compositeFuture -> promise.complete(new PackageDepsReport(compositeFuture.list())));
+        return promise.future();
+    }
 
     public static Future<PackageDepsReport> getPackageDependencies(String source) {
         FileSystem fileSystem = Vertx.vertx().fileSystem();
@@ -51,6 +74,7 @@ public class DependencyAnalyzerLib {
                 })
                 .compose(paths -> {
                     List<Future<ClassDepsReport>> classReports = paths.stream()
+                            .map(File::new)
                             .map(DependencyAnalyzerLib::getClassDependencies)
                             .collect(Collectors.toList());
                     return Future.all(classReports);
