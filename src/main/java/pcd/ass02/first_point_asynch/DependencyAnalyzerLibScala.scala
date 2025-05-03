6d18fd238073bf9d04da8b87a4f74b717b2f70bb
g-mainardi <giosue.mainardi@studio.unibo.it>
2025-05-03 16:08:03 +0200

    Asynch version: format refactor

--- src/main/java/pcd/ass02/first_point_asynch/DependencyAnalyzerLibScala.scala
+++ src/main/java/pcd/ass02/first_point_asynch/DependencyAnalyzerLibScala.scala
@@ -18,26 +18,25 @@ object DependencyAnalyzerLibScala {
   opaque type ProjectDepsReport = List[PackageDepsReport]
 
   private def isVisible(p: Path) = !(p.iterator.asScala map(_.toString) exists(n => n != "." && n.startsWith(".")))
-  private def isJavaFile(file: File) = file.getName.endsWith(".java")
+  private def isJavaFile(file: File) = file.getName endsWith ".java"
 
-  def getClassDependencies(source: File): Future[ClassDepsReport] = vertx.executeBlocking{() =>
-    val unit = parser.parse(source).getResult orElseThrow(() => new IllegalArgumentException("Failed to parse [" + source + "]"))
+  def getClassDependencies(source: File): Future[ClassDepsReport] = vertx executeBlocking{ () =>
+    val unit = parser.parse(source).getResult orElseThrow(() => IllegalArgumentException("Failed to parse [" + source + "]"))
     unit.getImports.asScala.toList map(_.getName) map(_.toString)
   }
 
-  def getPackageDependencies(source: File): Future[PackageDepsReport] =
-    vertx executeBlocking { () =>
-      source match
-        case s if !s.isDirectory => throw new IllegalArgumentException("This [" + source.toPath + "] is not a package")
-        case s => Option(s.listFiles) getOrElse (throw new IOException("List files returned null"))
-    } compose { files =>
-      val classReports = files filter (_.isFile) filter isJavaFile map getClassDependencies
-      Future all classReports.toList.asJava
-    } map {_.list.asScala.toList filter ((s: ClassDepsReport) => s.nonEmpty)}
+  def getPackageDependencies(source: File): Future[PackageDepsReport] = vertx executeBlocking { () =>
+    source match
+      case s if !s.isDirectory => throw IllegalArgumentException("This [" + source.toPath + "] is not a package")
+      case s                   => Option(s.listFiles) getOrElse (throw IOException("List files returned null"))
+  } compose { files =>
+    val classReports = files filter (_.isFile) filter isJavaFile map getClassDependencies
+    Future all classReports.toList.asJava
+  } map { _.list.asScala.toList filter ((s: ClassDepsReport) => s.nonEmpty) }
 
   def getProjectDependencies(source: File): Future[ProjectDepsReport] = vertx executeBlocking { () => source match
-    case s if !s.isDirectory => throw new IllegalArgumentException("Not a dir")
-    case s =>
+    case s if !s.isDirectory => throw IllegalArgumentException("Not a dir")
+    case s                   =>
       val stream = Files.walk(s.toPath)
       try (stream.iterator().asScala filter isVisible map(_.toFile) filter(_.isDirectory)).toSet
       finally stream.close()
