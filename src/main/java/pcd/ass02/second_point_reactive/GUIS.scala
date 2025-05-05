4c1f69014167065b925791ef23b7bf84d32f4884
g-mainardi <giosue.mainardi@studio.unibo.it>
2025-05-05 18:59:23 +0200

    Reactive: GUI refactor with preparation for graphPane

--- src/main/java/pcd/ass02/second_point_reactive/GUIS.scala
+++ src/main/java/pcd/ass02/second_point_reactive/GUIS.scala
@@ -1,36 +1,58 @@
 package pcd.ass02.second_point_reactive
 
+import Analyzer.{PackageInfo, scanProject}
 import javafx.application.Application
 import javafx.event.ActionEvent
-import javafx.scene.Scene
-import javafx.scene.control.*
-import javafx.scene.layout.BorderPane
-import javafx.scene.layout.HBox
-import javafx.stage.DirectoryChooser
-import javafx.stage.Stage
-import Analyzer.{PackageInfo, scanProject}
+import javafx.stage.{DirectoryChooser, Stage}
+import javafx.scene.{Scene, control, layout, input}
+import input.ScrollEvent.SCROLL
+import control.*
+import layout.{BorderPane, HBox, Pane}
 
 class GUIS extends Application {
+  val WIDTH = 800
+  val HEIGHT = 600
+  val hSpacing = 10
+
+  private lazy val graphPane = createGraphPane()
+  private var dragStartX = 0.0
+  private var dragStartY = 0.0
+  private def createGraphPane(): Pane =
+    val pane = Pane()
+    pane addEventFilter (SCROLL, e =>
+      val zoomFactor = if (e.getDeltaY > 0) 1.1 else 0.9
+      pane setScaleX(pane.getScaleX * zoomFactor)
+      pane setScaleY(pane.getScaleY * zoomFactor)
+      e.consume()
+    )
+    pane.setOnMousePressed {e =>
+      dragStartX = e.getSceneX - pane.getTranslateX
+      dragStartY = e.getSceneY - pane.getTranslateY
+    }
+    pane setOnMouseDragged {e =>
+      pane setTranslateX(e.getSceneX - dragStartX)
+      pane setTranslateY(e.getSceneY - dragStartY)
+    }
+    pane
+
   override def start(primaryStage: Stage): Unit =
-    val WIDTH = 800
-    val HEIGHT = 600
-    val hSpacing = 10
     val btnDir = Button("Select Source")
     val lblDir = Label("No folder selected")
     val btnRun = Button("Analyze")
     val lblClasses = Label("Classes: 0")
     val lblDeps = Label("Dependencies: 0")
-    val logArea = TextArea()
-    logArea setEditable false
     val topBar = HBox(hSpacing, btnDir, lblDir, btnRun, lblClasses, lblDeps)
     val root = BorderPane()
     root setTop topBar
-    root setCenter logArea
+    root setCenter graphPane
+
+    def drawPackageInfo(pi: PackageInfo): Unit = pi.log() //todo Implement real drawing
+
     btnDir setOnAction {_ =>
       Option(DirectoryChooser() showDialog primaryStage) match
         case Some(sel) =>
           lblDir setText sel.getAbsolutePath
-          btnRun setOnAction {_ => scanProject(sel).subscribe{(pi: PackageInfo) => pi.log()}}
+          btnRun setOnAction {_ => scanProject(sel) subscribe drawPackageInfo}
         case _ => throw IllegalArgumentException("File selection failed!")
     }
     primaryStage setScene Scene(root, WIDTH, HEIGHT)
