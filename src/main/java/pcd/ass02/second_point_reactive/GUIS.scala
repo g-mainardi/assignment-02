b7f181171fbf74b70098da393c6702f108316b48
g-mainardi <giosue.mainardi@studio.unibo.it>
2025-05-06 19:33:22 +0200

    Reactive: Graph extension methods

--- src/main/java/pcd/ass02/second_point_reactive/GUIS.scala
+++ src/main/java/pcd/ass02/second_point_reactive/GUIS.scala
@@ -6,6 +6,7 @@ import javafx.stage.{DirectoryChooser, Stage}
 import javafx.scene.{Scene, control, layout}
 import control.*
 import layout.{BorderPane, HBox}
+import org.graphstream.graph.{Edge, Node, Graph}
 import org.graphstream.graph.implementations.SingleGraph
 import org.graphstream.ui.fx_viewer.{FxViewPanel, FxViewer}
 import org.graphstream.ui.view.Viewer
@@ -26,16 +27,31 @@ class GUIS extends Application {
 
   private val classCounter = AtomicInteger(0)
   private val depCounter   = AtomicInteger(0)
+  extension (g: Graph)
+    private def addMyEdge(idFromTo: (String, ClassName | String, ClassName | String)): Edge =
+      g addEdge (idFromTo._1, idFromTo._2.toString, idFromTo._3.toString, true)
+    private def addMyEdge(fromTo: (ClassName | String, ClassName | String)): Edge =
+      g addMyEdge (edgeIdFormat(fromTo._1, fromTo._2), fromTo._1, fromTo._2)
+    private def addMyNode(id: ClassName | String): Node = g addNode id.toString
 
+  private def edgeIdFormat(from: ClassName | String, to: ClassName | String): String = s"$from->$to"
   private def drawClassNode(ci: ClassInfo): Unit =
-    val node = graph addNode ci.name.toString
+    val node = graph addMyNode ci.name
     node setAttribute("ui.label", ci.name)
-    if ci.packageName.nonEmpty then node setAttribute("ui.class", ci.packageName)
+    if (ci.packageName.nonEmpty)
+      val pkgClass = ci.packageName replace('.', '_')
+      node setAttribute("ui.class", pkgClass)
+      Option(graph getNode pkgClass) match
+        case Some(n) => graph addMyEdge (ci.packageName, ci.name)
+        case None    =>
+          val pkgNode = graph addMyNode pkgClass
+          pkgNode setAttribute("ui.label", ci.packageName)
+          pkgNode setAttribute("ui.class", pkgClass)
 
   private def drawDependency(from: ClassName, to: ClassName): Unit =
-    val edgeId: String = s"$from->$to"
+    val edgeId: String = edgeIdFormat(from, to)
     Option(graph getEdge edgeId) match
-      case None => graph addEdge(edgeId, from.toString, to.toString, true)
+      case None => graph addMyEdge (edgeId, from, to)
       case _    => ()
 
   override def start(primaryStage: Stage): Unit =
